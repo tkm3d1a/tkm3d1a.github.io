fd7660a56f54a3f8d41ca5279a16f2ec9fd1968e
Tim Klimpel <tim.klimpel@gmail.com>
2022-10-23 14:56:53 -0400

    Added template info and trying first test

---
+++ js/index.js
@@ -0,0 +1,19 @@
+//credit: https://developers.google.com/maps/documentation/javascript/adding-a-google-map#maps_add_map-javascript
+
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  const uluru = { lat: -25.344, lng: 131.031 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
+
+window.initMap = initMap;
