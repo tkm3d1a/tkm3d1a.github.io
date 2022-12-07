6460b6700820190dde12968be25b591283388dbb
Tim Klimpel <tim.klimpel@gmail.com>
2022-12-06 19:30:56 -0500

    Enabled navbar toggle and email function

--- js/index.js
+++ js/index.js
@@ -18,3 +18,14 @@ function initMap() {
 }
 
 window.initMap = initMap;
+
+
+// OnToggle
+function toggleFunction() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+  } else {
+      x.className = x.className.replace(" w3-show", "");
+  }
+}
